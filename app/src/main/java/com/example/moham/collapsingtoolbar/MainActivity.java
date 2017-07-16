7e82168526207a9f9da9f6c2df3a689a16a8609c
mohamedkhairy953 <mohamedsallam953@gmail.com>
2017-07-16 18:15:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/moham/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.moham.collapsingtoolbar;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar tot;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        tot = (Toolbar) findViewById(R.id.toto);
+
+    }
+}
